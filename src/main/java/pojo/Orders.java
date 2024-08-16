d575da4f0ad29131a70311cc0d239616aeb6acbe
a-levitt <artemis.levitt@gmail.com>
2024-08-16 14:12:57 +0200

    Create new order

---
+++ src/main/java/pojo/Orders.java
@@ -0,0 +1,14 @@
+package pojo;
+
+import java.util.List;
+
+public class Orders {
+    private List<OrderSingle> orders;
+
+    public List<OrderSingle> getOrders() {
+        return orders;
+    }
+    public void setOrders(List<OrderSingle> orders) {
+        this.orders = orders;
+    }
+}
--- src/test/java/shop/ECommerceApi.java
+++ src/test/java/shop/ECommerceApi.java
@@ -1,15 +1,17 @@
 package shop;
 
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
+import pojo.OrderSingle;
+import pojo.Orders;
 import secure.SecureUserData;
 import pojo.LoginResponse;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -29,7 +31,7 @@ public class ECommerceApi {
         given()
                 .spec(baseRequest)
                 .body(SecureUserData.loginData())
-         ;
+        ;
 
         LoginResponse loginResponse =
         reqLogin.when()
@@ -47,7 +49,7 @@ public class ECommerceApi {
                 .setBaseUri("https://rahulshettyacademy.com")
                 .addHeader("Authorization", token)
                 .build()
-                ;
+        ;
 
         RequestSpecification reqAddProduct =
         given()
@@ -60,7 +62,7 @@ public class ECommerceApi {
                 .param("productDescription", "Adidas shirt")
                 .param("productFor", "women")
                 .multiPart("productImage", new File("D:\\RESTAssured\\Images\\874ef0bd886ea8fefeb4f1d0be2f7d7e.jpg"))
-                ;
+        ;
 
         String productCreatedResponse =
         reqAddProduct.when()
@@ -69,8 +71,42 @@ public class ECommerceApi {
                 .extract().response().asString()
         ;
 
-        JsonPath js = new JsonPath(productCreatedResponse);
-        String lastProductId = js.get("productId");
+        JsonPath jsp = new JsonPath(productCreatedResponse);
+        String lastProductId = jsp.get("productId");
+
+        // Create Order
+        RequestSpecification createOrderBaseRequest = new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com")
+                .addHeader("Authorization", token)
+                .setContentType(ContentType.JSON)
+                .build()
+        ;
+
+        OrderSingle order = new OrderSingle();
+        order.setCountry("Montenegro");
+        order.setProductOrderedId(lastProductId);
+        List<OrderSingle> listOfOrders = new ArrayList<>();
+        listOfOrders.add(order);
+        Orders orders = new Orders();
+        orders.setOrders(listOfOrders);
+
+        RequestSpecification reqCreateOrder =
+        given()
+                .spec(createOrderBaseRequest)
+                .body(orders)
+        ;
+
+        String orderCreatedResponse =
+        reqCreateOrder.when()
+                .post("/api/ecom/order/create-order")
+        .then()
+                .extract().response().asString()
+        ;
+
+        JsonPath jso = new JsonPath(orderCreatedResponse);
+        String lastOrderId = jso.get("productOrderId[0]");
+
+        // View order details
 
     }
 }
